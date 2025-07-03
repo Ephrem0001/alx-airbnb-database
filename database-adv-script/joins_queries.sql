b359f2127e4c9379958f1daba6588b25fc8fa4be
Ephrem Mekonnen <ephrem8118@gmail.com>
2025-07-03 09:45:00 +0300

    Update joins_queries.sql

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -1,1 +1,14 @@
-
+SELECT
+    b.booking_id,
+    b.property_id,
+    b.start_date,
+    b.end_date,
+    u.user_id,
+    u.name As guest_name,
+    u.email
+FROM
+    booking b
+INNER JOIN
+    users u
+ON
+    b.guest_id = u.user_id;
